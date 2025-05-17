b985f1110e39d00664b8b8556b1ce560cf822031
karri <husnoraxamitova@gmail.com>
2025-05-17 10:23:29 +0500

    new code

---
+++ app.js
@@ -0,0 +1,57 @@
+let tg = window.Telegram.WebApp;
+tg.expand();
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#af0808";
+
+btn1.addEventListener("click", function (){
+    if (tg.MainButton.isVisible()){
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.show();
+    }
+})
+btn2.addEventListener("click", function (){
+    if (tg.MainButton.isVisible()){
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.show();
+    }
+})
+btn3.addEventListener("click", function (){
+    if (tg.MainButton.isVisible()){
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.show();
+    }
+})
+
+btn4.addEventListener("click", function (){
+    if (tg.MainButton.isVisible()){
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.show();
+    }
+})
+
+btn5.addEventListener("click", function (){
+    if (tg.MainButton.isVisible()){
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.show();
+    }
+})
+
+btn6.addEventListener("click", function (){
+    if (tg.MainButton.isVisible()){
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.show();
+    }
+})
+
--- keyboards.py
+++ keyboards.py
@@ -7,16 +7,14 @@ from aiogram.types import (
 )
 from messages import messages
 
-def telegram_web_app(lang):
-    url=" https://xusnora336.github.io/evos_bot/"
-    web_app = WebAppInfo(url=url)
-    return web_app
+url = "https://xusnora336.github.io/evos_bot/"
+
 def start_buttons(lang):
     buttons = ReplyKeyboardMarkup(keyboard=[
         [KeyboardButton(text=f"{messages[lang]['about_company']}"),
          KeyboardButton(text=f"{messages[lang]['branches']}")],
         [KeyboardButton(text=f"{messages[lang]['job_positions']}")],
-        [KeyboardButton(text=f"{messages[lang]['menu']}", web_app=telegram_web_app(lang)),
+        [KeyboardButton(text=f"{messages[lang]['menu']}", web_app=WebAppInfo(url=url)),
          KeyboardButton(text=f"{messages[lang]['news']}")],
         [
             KeyboardButton(text=f"{messages[lang]['contacts']}"),
